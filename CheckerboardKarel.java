7612b50dfbd104bb5ee5add64b348ab47cfddd33
S <s>
2017-03-02 22:02:51 -0800

    {"types":"C","runs":0}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -9,8 +9,33 @@
 
 import stanford.karel.*;
 
+/* algorithm for creating checkerboard : problem #4
+ * create an avenue variable: increase by one after each street completed
+ * if avenue is odd, will put down beepers starting on first avenue
+ * if avenue is even, will put down beepers starting on second avenue
+ * make sure to check if side is clear to continue creating checkerboard --> this should help with board size
+ */
+
 public class CheckerboardKarel extends SuperKarel {
 
-	// You fill in this part
+	int avenue = 1;
+	int length = 1;
+	
+	public void findOutLength() {
+		while(frontIsClear()) {
+			move();
+			length++;
+		}
+		turnAround();
+	}
+	public void checkerBoard() {
+		if (avenue % 2 != 0) {
+			putBeeperOnOddAvenue();
+		}
+	}
 
+	public void putBeeperOnOddAvenue() {
+		for (int i = 0; i )
+		
+	}
 }
